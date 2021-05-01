6e519cb83cf37e3f93958986abafe50e3af00a8f
MASTERGUY <murtipipo@gmail.com>
2021-05-01 15:14:31 +0530

    Upstream: Fixes of derps xD

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -744,7 +744,6 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 	pr_debug("bl_temp %d\n", bl_temp);
 	dsi = &panel->mipi_device;
 
-<<<<<<< HEAD
 	if (panel->bl_config.dcs_type_ss_ea || panel->bl_config.dcs_type_ss_eb)
 		rc = mipi_dsi_dcs_set_display_brightness_ss(dsi, bl_temp);
 	else
@@ -754,12 +753,6 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 	if (panel->bl_config.xiaomi_f4_41_flag)
 		rc = dsi_panel_update_backlight_demura_level(panel, bl_temp);
 
-=======
-	if (panel->bl_config.bl_inverted_dbv)
-		bl_lvl = (((bl_lvl & 0xff) << 8) | (bl_lvl >> 8));
-
-	rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
->>>>>>> e7fc409aa1b222ee5235d352a5d8f25814c74217
 	if (rc < 0)
 		pr_err("failed to update dcs backlight:%d\n", bl_temp);
 
